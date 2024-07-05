0eff6d5f26ef27438991081322be95a49b1aa63c
aohenuo's github account <72202049+aohenuo@users.noreply.github.com>
2024-07-05 17:47:35 +0800

    fix clip bug

--- icv_src/icv_encoder/clip_icv_encoder.py
+++ icv_src/icv_encoder/clip_icv_encoder.py
@@ -3,7 +3,7 @@ import torch
 from loguru import logger
 from torch import nn
 from transformers import AutoConfig, AutoModel, CLIPProcessor, CLIPModel
-
+from sklearn.decomposition import PCA
 
 class CLIPICVEncoder(BaseICVEncoder):
     def __init__(
@@ -32,7 +32,7 @@ class CLIPICVEncoder(BaseICVEncoder):
         self.icv = nn.ModuleList(
             [torch.nn.Linear(512, llm_hidden_dim) for _ in range(llm_layers)]
         )
-
+        self.clip_ft = clip_ft
         self.use_sigmoid = use_sigmoid
 
     def forward(self, data) -> ICVEncoderOutput:
@@ -46,14 +46,9 @@ class CLIPICVEncoder(BaseICVEncoder):
             icv = [
                 icv_layer(
                     torch.mean(
-                        outs.text_model_output.last_hidden_state[
-                            :,
-                            -1,
-                        ],
-                        dim=0,
-                        keepdim=True,
+                        torch.mean(outs.text_embeds,dim=0,keepdim=True)+torch.mean(outs.image_embeds,dim=0,keepdim=True),
+                        dim=0,keepdim=True)
                     )
-                )
                 for icv_layer in self.icv
             ]
             all_icv.append(icv)
@@ -69,7 +64,7 @@ class CLIPICVEncoder(BaseICVEncoder):
             return torch.sigmoid(self.alpha)
         return self.alpha
     
-    def load_weight(self,path,load_clip):
+    def load_weight(self,path):
         icv_weights = torch.load(path)
         for i, layer in enumerate(self.icv):
             layer_state_dict = {
@@ -78,12 +73,41 @@ class CLIPICVEncoder(BaseICVEncoder):
                     }
             layer.load_state_dict(layer_state_dict)
         self.alpha = torch.nn.Parameter(icv_weights['icv_encoder.alpha'], requires_grad=True)
-        if load_clip==True:
+        if icv_weights["clip_ft"] == True:
             new_state_dict = {}
             for key, value in icv_weights.items():
                 new_key = key.replace('icv_encoder.clip_embed.', '')
-                if new_key in icv_weights:
+                if new_key not in icv_weights:
                     new_state_dict[new_key] = value
-            self.clip_embed.load_state_dict(new_state_dict, strict=False)
+            self.clip_embed.load_state_dict(new_state_dict, strict=True)
         else:
             pass
+            
+def feature_extractor(text_feature, image_feature,mode):
+    if mode==1: ## Mean Aggregation
+        return torch.mean(
+                        torch.mean(text_feature,dim=0,keepdim=True)+torch.mean(image_feature,dim=0,keepdim=True),
+                        dim=0,keepdim=True)
+                    
+    elif mode==2: ## Weighted Average
+        ##todo
+        """
+        image_feature = 
+        text_feature =
+        self.image_weight = torch.nn.Parameter(torch.full(size=(1, 32),fill_value=1/32,
+            requires_grad=True)
+        self.text_weight = torch.nn.Parameter(torch.full(size=(1, 32),fill_value=1/32,
+            requires_grad=True)
+        image_feature = torch.mean(image_feature * self.image_weight, dim=0, keepdim=True)
+        text_feature = torch.mean(text_feature * self.text_weight, dim=0, keepdim=True)
+        return torch.mean(image_feature,text_feature,dim=0,keepdim=True)
+        """
+    
+    elif mode==3:##PCA
+        pca = PCA(n_components=1)
+        image_pca_result = pca.fit_transform(image_feature)
+        image_pca_vector = image_pca_result.components_[0]
+        text_pca_result = pca.fit_transform(text_feature)
+        text_pca_vector = text_pca_result.components_[0]
+        return torch.mean(image_pca_vector,text_pca_vector,dim=0,keepdim=True)
+    
